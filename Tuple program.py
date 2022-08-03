b9e71de18ef84db999bf049badb0f3e49454346b
Sai Harish <2100031942@kluniversity.in>
2022-08-03 16:13:08 +0530

    This is a Tuple Program

---
+++ Tuple program.py
@@ -0,0 +1,7 @@
+tuple = ('abcd',1234,"sai","Harish")
+tinytuple = (123 , "john")
+print(tuple)
+print(tuple[0])
+print(tuple[1:3])
+print(tinytuple*2) #repeats the list
+print(tuple+tinytuple) #prints concatenated tuples
